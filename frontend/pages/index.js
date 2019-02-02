1a65a25bdd27a674705445a7b2a5b6d0be266669
DESKTOP-TOOTMLU\DuyLongPc <LongTMGCS16297@fpt.edu.vn>
2019-02-02 14:30:24 +0700

    init

---
+++ frontend/pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+const Home = () => {
+    return (
+        <div>
+            Hello
+            <Link href="/sell">Sell
+            </Link>
+        </div>
+    );
+};
+
+export default Home;

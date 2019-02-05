7121c41090ba945ec6eec35109fcaa95d9daeb7f
tranminhlong0000 <tranminhlong0000@gmail.com>
2019-02-05 15:39:31 +0700

    delete Item

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5477f0e9f8be68ead2eb";
+/******/ 	var hotCurrentHash = "f69db672725ca431cad0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

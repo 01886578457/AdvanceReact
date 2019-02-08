3c8f53b067107670f8c95caff3dda91f45010222
tranminhlong0000 <tranminhlong0000@gmail.com>
2019-02-08 15:07:37 +0700

    finish Pagination

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4ff70c1bcf550d61d373";
+/******/ 	var hotCurrentHash = "2b4226be5eb9957027dd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

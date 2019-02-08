f68fc6b7d9882a81b62d0f740101231d0dd4486b
tranminhlong0000 <tranminhlong0000@gmail.com>
2019-02-08 15:34:25 +0700

    User Signup and Permission Flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const Mutations = {
     async createItem(parent, args, ctx, info) {
         //Todo: check if login
@@ -27,6 +29,30 @@ const Mutations = {
         //TODO
         //3. Execute delete
         return ctx.db.mutation.deleteItem({ where }, info)
+    },
+    async signup(parent, args, ctx, info) {
+        args.email = args.email.toLowerCase();
+        // hash password
+        const password = await bcrypt.hash(args.password, 10);
+        // create user in db
+        const user = await ctx.db.mutation.createUser({
+            data: {
+                ...args,
+                password,
+                permissions: { set: ['USER'] }
+            }
+        },
+            info
+        );
+        //create JWT Token
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+        //set the jwt on the response
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 60 * 60 //1 hour
+        });
+        //return the user
+        return user;
     }
 };
 
